0095df8d4fb5741b3e979390617ad9e30c713125
walonjalloh <mohamedlaminwalonjalloh@gmail.com>
2024-11-11 10:38:22 +0000

    add classwork.py

---
+++ intro_to_python/classwork.py
@@ -0,0 +1,21 @@
+#classwork for getting the largest number between three number
+
+print('Welcome to bigger number programme \n')
+
+a = int(input('Enter a number:\n'))
+b = int(input('Enter a number:\n'))
+c = int(input('Enter a number:\n'))
+
+#using the max function to get the largest number between a,b,c
+
+print('Bigger number is: \n')
+print(max(a,b,c))
+
+
+print('Bigger number is: \n')
+if(a >b and a > b):
+    print(a)
+elif(b>a and b>c):
+    print(b)
+else:
+    print(c)       
