1fdbf3954a541fd6d75faeb5a8a279c1562e9ffa
walonjalloh <mohamedlaminwalonjalloh@gmail.com>
2024-11-11 10:20:45 +0000

    add if-else and variable_mathoperation file

---
+++ intro_to_python/if-else.py
@@ -0,0 +1,34 @@
+# the class was thought the operation of if else elif ,
+# since we learnt about variable previously and the type of data types we used them in this class
+
+#defined variable
+
+myNum = 40
+myOptions = 'Yes'
+
+
+#practicing if,elif,else with the variable myNum
+if(myNum > 50):
+    print('greater than 20')
+elif (myNum < 30):   
+    print('less than 30')    
+else:
+    print('number is not applicable')   
+
+
+# practicing if,elif,else with the variable myOptiions
+if(myOptions == 'Yes'):
+    print('pass')
+else:
+    print('no')   
+
+
+
+# excercise to determine weather a number is even or odd
+
+num = 9
+
+if(num % 2 == 1):
+    print('odd')
+else:
+    print('even')    
---
+++ intro_to_python/variable_mathoperation.py
@@ -0,0 +1,34 @@
+# we learnt how to create a variable, understood data types,basic math operations and the rules in naming them
+
+myVar = 'Walon' #string data type
+myNumber = 89 # int data type
+myBool = False # boolean data type
+myFloat = 3.456 # float data type
+myChar = 'a' # char data type
+
+print(myVar)
+print(myNumber)
+print(myBool)
+print(myFloat)
+print(myChar)
+
+
+x = 9
+y = 10
+
+myTimes  = x * y # multiplication
+myAdd = x + y # adddition
+mySub = y - x  #subtraction
+myDivide = x / y  # divide
+myFloor = x//y #floor division
+myRemainder  = x % y # remainder division
+mySquare = x ** 2 or y **2 # gives the square of x or y
+
+print(myRemainder)
+print(myAdd)
+print(mySub)
+print(myDivide)
+print(myFloat)
+print(myFloor)
+print(mySquare)
+
