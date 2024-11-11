abafb483e6ffd7fe8f69f5d13e9221d0227fc22d
walonjalloh <mohamedlaminwalonjalloh@gmail.com>
2024-11-11 10:42:37 +0000

    add loops.py

--- intro_to_python/classwork.py
+++ intro_to_python/classwork.py
@@ -2,20 +2,21 @@
 
 print('Welcome to bigger number programme \n')
 
-a = int(input('Enter a number:\n'))
-b = int(input('Enter a number:\n'))
-c = int(input('Enter a number:\n'))
+a = int(input('Enter a number: '))
+b = int(input('Enter a number: '))
+c = int(input('Enter a number: '))
 
 #using the max function to get the largest number between a,b,c
 
-print('Bigger number is: \n')
+print('Bigger number is: ')
 print(max(a,b,c))
 
 
-print('Bigger number is: \n')
-if(a >b and a > b):
+print('Bigger number is: ')
+#using if,elif,else to determing the bigger number
+if(a > b and a > b):
     print(a)
-elif(b>a and b>c):
+elif(b > a and b > c):
     print(b)
 else:
     print(c)       
